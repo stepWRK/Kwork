39fd7e1c22188132b47359d235ab8d18a1d35743
StepWRK <stepapetrov089@gmail.com>
2025-10-25 14:05:35 +0700

    изменено для контрольной

--- main.py
+++ main.py
@@ -1,72 +1,74 @@
 import random
 
-print("""напишите 2 значения выс и шир (мин 6)
-пустоты рядом с берегами являются лужами
-рекоммендованные размеры: 90 x 90
-——————————————————————————————————————————————""")
-width = int(input("Ширина области: "))
-height = int(input("Высота области: "))
-Isl = [["ㅤ" for _ in range(width)] for _ in range(height)]
-tudaX = width // 2
-tudaY = height // 2
-Isl[tudaY][tudaX] = "▨"
-KudaSuda = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-XEd = random.randint(width * height // 4, width * height // 2)
-Used = 0
+# задача C (сложное)
+#
 
-# справочник по переменным
-# Isl - острове
-# tudaX/Y - ширина и выс терр для ген
-# KudaSuda - направления для ген
-# XEd - еденицы
-# Used - использованно
-while Used < XEd:
-    y = random.randint(1, height - 2)
-    x = random.randint(1, width - 2)
-    if Isl[y][x] == "▨":
-        dx, dy = random.choice(KudaSuda)
-        tudaX, tudaY = x + dx, y + dy
-        if 1 <= tudaX < width - 1 and 1 <= tudaY < height - 1:
-            if Isl[tudaY][tudaX] == "ㅤ":
-                Isl[tudaY][tudaX] = "▨"
-                Used += 1
 
-print("\nСлучайный остров:")
-for StR in Isl:
-    print("".join(StR))
+print("""
 
-#Генерация озёр —————————————————————————————————————————————————————
+задание Ⅰ (1)
+треугольник △
+———————————————————————————————————————————————————————————————————————————————————————————————————————————————""")
+print("введите размер треугольника")
+SizTriangle = int(input())                   #triangle - треугольник, гений
 
-# Генерация клада ————————————————————————————————————————————————————
-KladGen = False
-Attem = 0
-MAttem = 1000
-while not KladGen and Attem < MAttem:
-    y = random.randint(1, height - 2)
-    x = random.randint(1, width - 2)
-    if Isl[y][x] == "▨" and (y != height // 2 or x != width // 2):
-        is_safe = True
-        for dx, dy in KudaSuda:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < width and 0 <= ny < height:
-                if Isl[ny][nx] == "ㅤ":
-                    is_safe = False
-                    break
-        if is_safe:
-            Isl[y][x] = "▣"
-            KladGen = True
-    Attem += 1
+for i in range(SizTriangle):
+    if i == 0:
+        print(" " * (SizTriangle - 1) + "*")
+    elif i == SizTriangle - 1:               #заполнение низней части треугольника
+        print("*" * (2 * SizTriangle -1))
+    else:
+        SpaceBef = SizTriangle - i - 1
+        SpaceBetw = 2 * i - 1
+        print(" " * SpaceBef + "*" + " " * SpaceBetw + "*")
+print("""
 
-print("\nостров с озером и кладом:")
-for StR in Isl:
-    print("".join(StR))
 
-if OzerGen:
-    print("\nОзеро ~ размещено")
+задача Ⅱ (2)
+поля случайных чисел
+———————————————————————————————————————————————————————————————————————————————————————————————————————————————""")
+print("""
+введите число для проверки""")
+num = int(input())
+Anum = 10
+Bnum = 50
+if Anum <= num <=Bnum:
+    print("число принадлежит промежутку")
 else:
-    print("\nОзеро не размещено, попробуй ещё раз")
+    print("число не принадлежит промежутку")
 
-if KladGen:
-    print("Клад ▣ размещён")
-else:
-    print("Клад не размещён, попробуй ещё раз")
+print("""
+
+задача Ⅲ (3)
+поля случайных чисел
+———————————————————————————————————————————————————————————————————————————————————————————————————————————————""")
+SizInp = int(input())
+SideX = SizInp
+UpX = SizInp
+Matrix = []
+
+print("исходная матрица")
+for i in range(SideX):
+    UpGlob = [random.randint(1, 99) for _ in range(SideX)]
+    Matrix.append(UpGlob)
+    print(" ".join(f"{num:2}" for num in UpGlob))
+
+while True:
+    print("Команды: влево (1) и выход (2)")
+    Command = input("""
+Введите команду:""").lower()
+
+    if Command == "2":
+        print("Goodbye world! *deleting folder system_32*")
+        break
+    elif Command == "1":
+        shifted_matrix = []
+        for row in Matrix:
+            shifted_row = row[1:] + [row[0]]
+            shifted_matrix.append(shifted_row)
+        Matrix = shifted_matrix
+    else:
+        print("поздравляю, ваш средний IQ чуть больше чем у Чихуахуа")
+        continue
+    for row in Matrix:
+        print(" ".join(f"{num:2}" for num in row))
